610385e768e805ae6e564b0577fc04cdcc07fd5f
yinfanliang <554665488@qq.com>
2017-06-27 10:30:26 +0800

    建立function文件-stream_context_create

--- length.php
+++ length.php
@@ -4,8 +4,6 @@ header('Content-type:text/html;charset=utf-8');
 
 * 中文截取，支持gb2312,gbk,utf-8,big5
 
-*
-
 * @param string $str 要截取的字串
 
 * @param int $start 截取起始位置
@@ -71,3 +69,40 @@ function abslength($str)
         return count($ar[0]);
     }
 }
+
+/*
+    utf-8编码下截取中文字符串,参数可以参照substr函数
+    @param $str 要进行截取的字符串
+    @param $start 要进行截取的开始位置，负数为反向截取
+    @param $end 要进行截取的长度
+*/
+function utf8_substr($str,$start=0) {
+    if(empty($str)){
+        return false;
+    }
+    if (function_exists('mb_substr')){
+        if(func_num_args() >= 3) {
+            $end = func_get_arg(2);
+            return mb_substr($str,$start,$end,'utf-8');
+        }
+        else {
+            mb_internal_encoding("UTF-8");
+            return mb_substr($str,$start);
+        }      
+ 
+    }
+    else {
+        $null = "";
+        preg_match_all("/./u", $str, $ar);
+        if(func_num_args() >= 3) {
+            $end = func_get_arg(2);
+            return join($null, array_slice($ar[0],$start,$end));
+        }
+        else {
+            return join($null, array_slice($ar[0],$start));
+        }
+    }
+}
+$str2  = 'wo要截取zhongwen';
+echo '<br />';
+echo utf8_substr($str2,0,-4); //return wo要截取zhon
--- reflect/index.php
+++ reflect/index.php
@@ -1,3 +1,3 @@
 <?php
 require_once('./autoload.php');
- $a=new aaaaaa();
+class 
