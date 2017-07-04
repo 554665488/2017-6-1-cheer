34505c2ead7163e302733fbd0ffa771f5202632f
yinfanliang <554665488@qq.com>
2017-07-04 16:27:03 +0800

    PHP中$this和$that指针使用实例 $this 指的复本$person2， 而$that 是指向原本$person，

--- clone.php
+++ clone.php
@@ -35,7 +35,9 @@
         public function __clone() {    #复制方法,可在里面定义再clone是进行的操作
             // $this 指的复本$person2， 而$that 是指向原本$person，这样就在本方法里，改变了复本的属性。
             $this->id = 0;
+
             $this->account = clone $this->account;    #不加这一句,account在clone是会只被复制引用,其中一个account的balance被修改另一个也同样会被修改
+            $this->account->balance=50000;
         }
     }
     
@@ -43,7 +45,7 @@
     $person->setId(1);
     $person2 = clone $person;
     
-    $person2->account->balance = 250;
+//    $person2->account->balance = 250;
     
     var_dump($person);
 	echo "</br>";
